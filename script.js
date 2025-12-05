1ccaf8a3238930e262724db86aee5c24d0cd7a87
mehrabkb <mkordbacheh75@gmail.com>
2025-12-05 12:54:17 +0330

    add typer js to project

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 AOS.init();
+
+$(function(){
+    let myInterval;
+
+    // شروع interval
+    function startInterval() {
+        if (myInterval) clearInterval(myInterval);
+        myInterval = setInterval(() => {
+            $("#typer1").empty();
+          $("#typer1").typing({
+            sourceElement: $("#typer1-content")
+          });
+          setTimeout(stopInterval,4000);
+        }, 3000);
+    }
+
+    // توقف interval
+    function stopInterval() {
+        // $("#typer1").empty();
+        clearInterval(myInterval);
+        startInterval();
+    }
+
+    // استفاده:
+    startInterval();              // شروع میشه
+    setTimeout(stopInterval, 500); // بعد از 5 ثانیه متوقف میشه
+    setTimeout(startInterval, 800); // بعد از 8 ثانیه دوباره شروع میشه
+})
